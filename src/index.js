6c8578456f582191f8e5b23dd313262616f6089b
calvin <ncalvin67@gmail.com>
2023-03-06 15:01:39 +0300

    :zap: Adding context providers

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { StateProvider } from "./context/stateProvider";
+import { initialState } from "./context/initialState";
+import reducer from "./context/reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
+    <StateProvider initialState={initialState} reducer={reducer}>
       <App />
+    </StateProvider>
   </Router>
 );
