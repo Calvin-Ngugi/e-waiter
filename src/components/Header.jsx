783e5020e1151ba18a7818d7c15fd038dc538b84
calvin <ncalvin67@gmail.com>
2023-03-06 10:31:05 +0300

    :wrench: Don't add env files to git

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
+import { motion } from "framer-motion";
 import React from "react";
 import {MdShoppingBasket} from "react-icons/md"
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="fixed z-50 w-screen p-6 px-16">
       {/* desktop & tablet*/}
       <div className="hidden md:flex w-full h-full justify-between">
-        <div className="flex items-center gap-2">
+        <Link to={'/'} className="flex items-center gap-2">
             <img src="img/logo.png" alt="logo" className="w-8 object-cover" />
             <p className="text-headingColor text-xl font-bold">E-waiter</p>
-        </div>
+        </Link>
 
         <div className="flex gap-7">
         <ul className="flex gap-8 items-center">
@@ -25,7 +27,7 @@ const Header = () => {
             <p className="text-xs text-white font-semibold">2</p>
           </div>
         </div>
-        <img src="img/avatar.png" alt="user-profile" className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer" />
+        <motion.img whileTap={{scale:0.6}} src="img/avatar.png" alt="user-profile" className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer" />
         </div>
       </div>
     </header>
